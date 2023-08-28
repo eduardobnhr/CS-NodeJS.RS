8cce53f88d53e7c573d0f7369c3f53c118b8dc6e
eduardobnhr <eduardobnhr@gmail.com>
2023-08-28 15:16:29 -0400

    inicio de pacotes

---
+++ iniciandoPratica/exports.js
@@ -0,0 +1,8 @@
+
+//criar um modulo
+
+module.exports = "enviando dados para o modulo"
+
+//meus modulos
+//const myModule = require('./exorts')
+//console.log(myModule)
---
+++ iniciandoPratica/process.js
@@ -0,0 +1,4 @@
+//lista de argumentos
+
+console.log(process.argv)
+//console.log('seu nome é', process.argv[2] + ' ' + process.argv[3])
